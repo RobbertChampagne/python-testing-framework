5d44161003ff8669d5db00813df004c21e21a631
Robbert <robbert.mediahuis@gmail.com>
2024-11-02 11:56:08 +0100

    module b

--- tests/api/module_b/setup/get_image_id.py
+++ tests/api/module_b/setup/get_image_id.py
@@ -1,7 +1,7 @@
-# python tests/api/module_b/setup/get_images.py
-
+import json
 import httpx
 from .cognito_token import get_cognito_token
+from ...core.apis_info import ApiAbbreviation, apiUrls
 
 async def get_image_id():
     headers = {
@@ -9,11 +9,11 @@ async def get_image_id():
         'Accept': 'application/json'
     }
     async with httpx.AsyncClient() as client:
-        url = "https://api.thecatapi.com/v1/breeds?limit=2&page=0"
+        url = apiUrls[ApiAbbreviation.TheCatApi] + "/breeds?limit=2&page=0"
         response = await client.get(url, headers=headers)
 
     breeds  = response.json()
     image_id = breeds[0]["image"]["id"]
-    #print(json.dumps(breeds, indent=4))
+    print(json.dumps(breeds, indent=4))
     return image_id
     
